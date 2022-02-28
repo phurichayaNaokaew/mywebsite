d97935d0e0d9ebea0a958ddedb3f4a4eccb7ab02
phurichayaNaokaew <100582591+phurichayaNaokaew@users.noreply.github.com>
2022-02-28 22:13:49 +0700

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#box1{
+				width: 800px;
+				height: 350px;
+			}
+			.box{
+				background-color: blue;
+			}
+			body{
+                                background-color: lightpink;
+			}
+			h1{
+				color: yellow;      
+		</style>
+	</head>
+	<body>
+		<div id="box1" class="box">
+			<h1>นางสาวภูริชญา เนาว์แก้ว ม4/13 เลขที่ 12</h1>
+		</div>
+	</body>
+</html>
