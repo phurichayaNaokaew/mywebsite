2ddb51769f4f2212c8de81f2240c33ac7dc5db8c
phurichayaNaokaew <100582591+phurichayaNaokaew@users.noreply.github.com>
2022-02-28 22:06:04 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,43 @@
+<html>
+  <head>
+        <title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+        <style>
+            #box1{
+                width: 800px;
+                height: 350px;
+                background-color:lightblue
+            }
+            #box2{
+                width: 600px;
+                height: 150px;
+            }
+            body{
+                background-color: pink;
+            }
+        </style>
+    </head>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("iannnnn-DUCK-Bold.ttf");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("Uranus.ttf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">ภูริชญา เนาว์แก้ว เลขที่ 12 เลขประจำตัว 57001</h1>
+    <h1 id="font2">ภูริชญา เนาว์แก้ว เลขที่ 12 เลขประจำตัว 57001</h1>
+  </body>
+</html>
+เขียนถึง Parinya Manuch
