203f3e80107073af69c337f29f10089e079dd097
phurichayaNaokaew <100582591+phurichayaNaokaew@users.noreply.github.com>
2022-03-06 11:59:29 +0700

    Update table.php

--- table.php
+++ table.php
@@ -1,1 +1,53 @@
-
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>พัชร์ธีรัตน์ แหลมหลวง</td>
+        <td>สไปรท์ บะบะบิ </td>
+        <td><img src="https://sv1.siamnews.com/img_1/13/210923pxo9i.jpg" width="145px"></td>
+        <td><a href="https://twitter.com/SpriteBABABI?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+      <tr>
+        <td>2</td>
+       <td>รังสิมันต์ โรม</td>
+        <td>โรม</td>
+        <td><img src="https://images.workpointnews.com/workpointnews/2019/07/25160826/1564045702_83044_3.jpg" width="145px"></td>
+        <td><a href="https://twitter.com/RangsimanRome">twitter</a>
+      <tr>
+        <td>3</td>
+       <td>หยาดพิรุณ ปู่หลุ่น</td>
+        <td>น้ำหยาด</td>
+        <td><img src="https://static.trueplookpanya.com/tppy/member/m_545000_547500/545920/cms/images/%E0%B8%AB%E0%B8%A2%E0%B8%B2%E0%B8%94%E0%B8%9E%E0%B8%B4%E0%B8%A3%E0%B8%B8%E0%B8%9309.jpg" width="145px"></td>
+        <td><a href="https://twitter.com/yardpirunpoolun">twitter</a></td>
+      <tr>
+        <td>4</td>
+        <td>ปราโมทย์ ปาทาน</td>
+        <td>โอ๊ต</td>
+        <td><img src="https://www.thebangkokinsight.com/wp-content/uploads/2021/10/batch_%E0%B9%82%E0%B8%AD%E0%B9%8A%E0%B8%95-%E0%B8%9B%E0%B8%A3%E0%B8%B2%E0%B9%82%E0%B8%A1%E0%B8%97%E0%B8%A2%E0%B9%8C2.jpg" width="145px"></td>
+        <td><a href="https://twitter.com/oatpramote">twitter</a></td>
+       <tr>
+        <td>5</td>
+        <td>วิโรจน์ ลักขณาอดิศร</td>
+        <td>วิโรจน์</td>
+        <td><img src="https://www.matichon.co.th/wp-content/uploads/2020/02/%E0%B8%A7%E0%B8%B4%E0%B9%82%E0%B8%A3%E0%B8%88%E0%B8%99%E0%B9%8C-%E0%B8%A5%E0%B8%B1%E0%B8%81%E0%B8%82%E0%B8%93%E0%B8%B2%E0%B8%AD%E0%B8%94%E0%B8%B4%E0%B8%A8%E0%B8%A3-2.jpg" width="145px"></td>
+        <td><a href="https://twitter.com/wirojlak">twitter</a></td> 
+       <tr>
+        <td>6</td>
+        td>วัชระ สุขชุม</td>
+        <td>เจนนี่</td>
+        <td><img src="https://s359.kapook.com/pagebuilder/13ec4d63-a964-4c0b-bfe9-23948a323d2b.png" width="145px"></td>
+        <td><a href="https://twitter.com/JenniePanhan?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a> 
+        </tr>  
+    </table>
+  </body> 
+</html>
